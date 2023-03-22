f64927b251c5e43cde35bc547c296982eb043603
iota9star <iota9star@foxmail.com>
2023-03-22 15:08:39 +0800

    🔖 v0.1.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.5
+
+- Use `const`.
+
 ## 0.1.4
 
 - Add `class` comments.
--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,3 @@
 import 'package:env2dart/env2dart.dart';
 
-import 'env.g.dart';
-
 void main() => parseAndGen(["-o", "./lib/env.g.dart"]);
-
-a(){
-  var env = Env();
-}
